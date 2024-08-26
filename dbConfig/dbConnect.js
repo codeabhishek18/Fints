7be4307e55b8ba5ad5fac46dfd80f0b2539f2a07
Abhishek PM <codeabhishek18@gmail.com>
2024-08-26 06:51:02 +0530

    Rebuild

--- dbConfig/dbConnect.js
+++ dbConfig/dbConnect.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
+if (!MONGODB_URI) 
+{
   throw new Error(
     'Please define the MONGODB_URI environment variable inside .env.local'
   );
@@ -10,24 +11,31 @@ if (!MONGODB_URI) {
 
 let cached = global.mongoose;
 
-if (!cached) {
+if (!cached) 
+{
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-async function dbConnect() {
-  if (cached.conn) {
+async function dbConnect() 
+{
+  if (cached.conn) 
+  {
     return cached.conn;
   }
 
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
+  if (!cached.promise) 
+  {
+    const opts = 
+    {
+      serverSelectionTimeoutMS: 5000,
     };
 
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => 
+    {
       return mongoose;
     });
   }
+  
   cached.conn = await cached.promise;
   return cached.conn;
 }
